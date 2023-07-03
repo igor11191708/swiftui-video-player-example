ee8b18fe8b80414f553bd6b6f8e60984330fe597
Igor <igorshelopaev@gmail.com>
2023-07-03 13:28:15 +0500

    upload

--- swiftui-loop-videoplayer-example/ContentView.swift
+++ swiftui-loop-videoplayer-example/ContentView.swift
@@ -6,21 +6,71 @@
 //
 
 import SwiftUI
+import swiftui_loop_videoplayer
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            NavigationLink(destination: Video())
+            {
+                labelTpl("display", color: .green)
+            }
+            NavigationLink(destination: Video1())
+            {
+                labelTpl("video")
+            }
+            NavigationLink(destination: Video2())
+            {
+               labelTpl("airplayvideo.circle")
+            }
+            NavigationLink(destination: Video3())
+            {
+                labelTpl("e.circle", color: .red)
+            }
         }
-        .padding()
+    }
+    
+    @ViewBuilder
+    private func labelTpl(_ name : String, color : Color = .blue) -> some View{
+        Image(systemName: name)
+            .font(.system(size: 50))
+            .padding(8)
+            .foregroundColor(color)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct Video1 : View{
+    var body: some View{
+        VStack {
+           PlayerView(resourceName: "swipe")
+                .frame(width: 300, height: 239)
+                .offset(x: 102, y: -25)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct Video2 : View{
+    var body: some View{
+        ZStack {
+           PlayerView(resourceName: "swipe")
+        }
+    }
+}
+
+struct Video3 : View{
+    var body: some View{
+        ZStack(alignment: .center) {
+           PlayerView(resourceName: "swipe_")
+        }
+    }
+}
+
+
+struct Video : View{
+    var body: some View{
+        ZStack(alignment: .center) {
+            PlayerView(resourceName: "swipe", videoGravity: .resizeAspectFill)
+        }.ignoresSafeArea()
     }
 }
--- swiftui-loop-videoplayer-example/swiftui_loop_videoplayer_exampleApp.swift
+++ swiftui-loop-videoplayer-example/swiftui_loop_videoplayer_exampleApp.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 @main
 struct swiftui_loop_videoplayer_exampleApp: App {
+    
+    /// Delegate for processing notifications and  events of app level
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    /// Application enter point
+    /// - Parameters:
+    ///   - application: app
+    ///   - launchOptions: launch params
+    /// - Returns: State
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+
+        setupAudio()
+
+        return true
+    }
+
+    /// Setup audio params
+    func setupAudio() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, mode: AVAudioSession.Mode.default, options: [.mixWithOthers])
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("something went wrong")
         }
     }
 }
