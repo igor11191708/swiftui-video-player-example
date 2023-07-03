01e26662f35192a2f0f25d6cd7041c076af8a571
Igor <igorshelopaev@gmail.com>
2023-07-03 12:01:48 +0500

    Initial Commit

---
+++ swiftui-loop-videoplayer-example/swiftui_loop_videoplayer_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_loop_videoplayer_exampleApp.swift
+//  swiftui-loop-videoplayer-example
+//
+//  Created by Igor on 03.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_loop_videoplayer_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
