7f96f127a457da2fad8c7543a4f7c4e0c27fad04
Igor <igorshelopaev@gmail.com>
2024-08-06 09:29:54 +0200

    Update ContentView.swift

--- swiftui-loop-videoplayer-example/ContentView.swift
+++ swiftui-loop-videoplayer-example/ContentView.swift
@@ -29,7 +29,7 @@ struct ContentView: View {
                         {
                             labelTpl("video")
                         }
-                        NavigationLink(destination: Video3())
+                        NavigationLink(destination: Video2())
                         {
                             labelTpl("airplayvideo.circle")
                         }
