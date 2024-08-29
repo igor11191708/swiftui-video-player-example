9a856cf022cb72c3b92814c116be92ac32426c80
Igor <igorshelopaev@gmail.com>
2024-08-29 18:29:05 +0200

    update

---
+++ VideoPlayerUITests/ContentViewUITests.swift
@@ -0,0 +1,16 @@
+//
+//  ContentViewUITests.swift
+//  VideoPlayerUITests
+//
+//  Created by Igor  on 29.08.24.
+//
+
+import XCTest
+@testable import swiftui_loop_videoplayer_example
+
+class ContentViewTests: XCTestCase {
+    
+    func testNavigationToFirstVideo() throws {
+
+    }
+}
--- swiftui-loop-videoplayer-example/ContentView.swift
+++ swiftui-loop-videoplayer-example/ContentView.swift
@@ -9,50 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State public var navigationPath = NavigationPath()
+
     var body: some View {
-            NavigationStack{
-                ZStack{
-                    ResponsiveStack(spacing: 5) {
-                        Spacer()
-                        NavigationLink(destination: Video())
-                        {
-                            labelTpl("display", color: .green)
-                        }
-                        NavigationLink(destination: Video1())
-                        {
-                            labelTpl("video")
-                        }
-                        NavigationLink(destination: Video2())
-                        {
-                            labelTpl("airplayvideo.circle")
-                        }
-                        
-                        NavigationLink(destination: Video6())
-                        {
-                            labelTpl("appletvremote.gen2")
-                        }
-                        NavigationLink(destination: Video8())
-                        {
-                            labelTpl("cloud")
-                        }
-                        NavigationLink(destination: Video3())
-                        {
-                            labelTpl("e.circle", color: .red)
-                        }
-                        Spacer()
-                    }
+        NavigationStack(path: $navigationPath) {
+            ResponsiveStack(spacing: 5) {
+                Spacer()
+                NavigationLink(value: "Video") {
+                    labelTpl("display", color: .green)
                 }
-                .frame(maxWidth: .infinity)
-                .background(.quaternary)
-                .ignoresSafeArea()
-            }.preferredColorScheme(.dark)
+                .accessibilityIdentifier("Video")
+                NavigationLink(value: "Video1") {
+                    labelTpl("video")
+                }
+                NavigationLink(value: "Video2") {
+                    labelTpl("airplayvideo.circle")
+                }
+                NavigationLink(value: "Video6") {
+                    labelTpl("appletvremote.gen2")
+                }
+                NavigationLink(value: "Video8") {
+                    labelTpl("cloud")
+                }
+                NavigationLink(value: "Video3") {
+                    labelTpl("e.circle", color: .red)
+                }
+                Spacer()
+            }
+            .background(.quaternary)
+            .ignoresSafeArea()
+            .navigationDestination(for: String.self) { item in
+                switch item {
+                case "Video":
+                    Video()
+                case "Video1":
+                    Video1()
+                case "Video2":
+                    Video2()
+                case "Video6":
+                    Video6()
+                case "Video8":
+                    Video8()
+                case "Video3":
+                    Video3()
+                default:
+                    EmptyView()
+                }
+            }
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
-// MARK: - Fileprivate
-
+// ViewBuilder for labels
 @ViewBuilder
-fileprivate func labelTpl(_ name : String, color : Color = .blue) -> some View{
+fileprivate func labelTpl(_ name: String, color: Color = .blue) -> some View {
     Image(systemName: name)
         .font(.system(size: 68))
         .padding(8)
