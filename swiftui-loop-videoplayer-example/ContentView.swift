25e921471ebe0c051f17dcf824f084cf1842b405
Igor <igorshelopaev@gmail.com>
2024-09-13 10:02:42 +0200

    Update ContentView.swift

--- swiftui-loop-videoplayer-example/ContentView.swift
+++ swiftui-loop-videoplayer-example/ContentView.swift
@@ -31,7 +31,7 @@ struct ContentView: View {
         }
         .preferredColorScheme(.dark)
         .onAppear{
-              navigationPath = .init(["Video8"])
+             // navigationPath = .init(["Video8"])
         }
     }
     
